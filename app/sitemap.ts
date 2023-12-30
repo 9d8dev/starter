2fcd2009f689d3025e51a9b1823330f7f7632b72
Bridger Tower <bridgertower@gmail.com>
2023-12-30 11:29:26 -0700

    update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,21 +1,22 @@
 import { MetadataRoute } from "next";
+import site from "@/site.config";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://acme.com",
+      url: site.url,
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://acme.com/about",
+      url: `${site.url}/about`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://acme.com/blog",
+      url: `${site.url}/blog`,
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.5,
