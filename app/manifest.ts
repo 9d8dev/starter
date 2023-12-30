186c0a1e98de059db78edfe981f300f2c53ef7de
Bridger Tower <bridgertower@gmail.com>
2023-12-30 11:27:32 -0700

    update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,10 +1,11 @@
 import { MetadataRoute } from "next";
+import site from "@/site.config";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "Next.js App",
-    short_name: "Next.js App",
-    description: "Next.js App",
+    name: site.name,
+    short_name: site.app_name,
+    description: site.description,
     start_url: "/",
     display: "standalone",
     background_color: "#fff",
