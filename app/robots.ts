724f9fd60c7363b53815100c8cc6ae70baeb3d5e
Bridger Tower <bridgertower@gmail.com>
2023-12-30 11:28:09 -0700

    update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,4 +1,5 @@
 import { MetadataRoute } from "next";
+import site from "@/site.config";
 
 export default function robots(): MetadataRoute.Robots {
   return {
@@ -7,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/private/",
     },
-    sitemap: "https://acme.com/sitemap.xml",
+    sitemap: `${site.url}/sitemap.xml`,
   };
 }
