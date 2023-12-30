e74f5c60fa68f5b577fbe1f9c4a26fde4b3bab4d
Bridger Tower <bridgertower@gmail.com>
2023-12-30 10:48:14 -0700

    update link

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://9d8.dev"
             target="_blank"
             rel="noopener noreferrer"
           >
