9f87a8f218e9564e31e84705a9bb575887ada8fe
Cameron Youngblood <cameron.youngblood@gmail.com>
2024-01-02 16:10:08 -0700

    fix: uncomment out resend

--- lib/resend.ts
+++ lib/resend.ts
@@ -1,4 +1,3 @@
 import { Resend } from "resend";
 
-// Uncomment the following line to use Resend
-// export const resend = new Resend(process.env.RESEND_API_KEY || "");
+export const resend = new Resend(process.env.RESEND_API_KEY || "");
